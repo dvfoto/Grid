a6fa2e0b342622a75f89f5d7fd9f89d8720ce197
Deividas <vilimaitisdeividas@gmail.com>
2022-05-14 11:39:17 +0300

    as

---
+++ java.js
@@ -0,0 +1,31 @@
+console.log('labas vakaras');
+
+let x;  
+let saldainiai = 'Fortuna';
+x = 10;
+
+console.log(typeof saldainiai, typeof x);
+// var senas kintamojo zymejimas galima be tvarkos
+//let kintamasis turi tvarka
+let petrius;
+let one;
+let kas;
+
+
+// dakyba su trukmena
+kas = 17%3
+console.log('liekana', kas)
+kas++
+console.log('liekana', kas)
+kas--;
+console.log('liekana', kas)
+petrius = '1';
+one= x / 2;
+
+console.log(typeof petrius, 'Petrius', petrius, 'One', one)
+
+let loginis = true
+console.log(loginis)
+let loginis1 = false
+
+console.log(loginis1)
